a8d0d560cc1c7f6f92cc7dfad797a27454c15e61
Kurbanov Rustam <kurbanov.rustam@applecity.kz>
2020-03-04 12:35:03 +0600

    Fix style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -109,9 +109,11 @@
             </section>
         </main>
     </main>
+    <section>
         <div class="map-block">
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3446.5206248564004!2d-97.75646998447864!3d30.25074398180767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b5d05ff14a83%3A0xd534ee3eada9f293!2s1311%20S.%201st%20St.%20Park%2C%20Ira%20and%20Bev&#39;s!5e0!3m2!1sru!2skz!4v1583124153853!5m2!1sru!2skz"
-                width="1920" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
+    </section>
 @endsection
